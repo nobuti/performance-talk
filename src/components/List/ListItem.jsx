c46a201deebaa72250e0f7e3fe1c313884a103c9
nobuti <buti@nobuti.com>
2021-10-12 18:24:31 +0200

    Show skull

--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -6,11 +6,11 @@ const noop = () => {};
 const ListItem = ({ year, name, deaths, id, onClick = noop }) => (
   <div className={styles.item} onClick={() => onClick(id)}>
     <div className={styles.left}>{name}</div>
-    {/* <div className={styles.center}>
+    <div className={styles.center}>
       {deaths > 0 ? (
         <img className={styles.icon} src="/skull.svg" alt="Deathly" />
       ) : null}
-    </div> */}
+    </div>
     <div className={styles.right}>{year}</div>
   </div>
 );
