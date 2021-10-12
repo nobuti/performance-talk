d4df714335ef78485e026324b1ecc10f69f3b4cb
nobuti <buti@nobuti.com>
2021-10-12 18:18:17 +0200

    Virtualize

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { memo } from "react";
+import { FixedSizeList as VirtualisedList } from "react-window";
 
 import ListItem from "./ListItem";
 
 import styles from "./styles.module.css";
 
-const List = ({ volcanos, volcanoHandler }) => (
-  <div className={styles.list}>
-    <div className={styles.header}>
-      <div className={styles.left}>Name</div>
-      <div className={styles.center}>Deaths</div>
-      <div className={styles.right}>Year</div>
-    </div>
+const List = memo(({ volcanos, volcanoHandler }) => {
+  const height = Math.min(
+    document.body.clientHeight - 178,
+    volcanos.length * 60
+  );
 
-    {volcanos.map((volcano) => (
-      <ListItem key={volcano.id} onClick={volcanoHandler} {...volcano} />
-    ))}
-  </div>
-);
+  const Row = (props) => (
+    <ListItem volcanoHandler={volcanoHandler} {...props} />
+  );
+
+  return (
+    <div className={styles.list}>
+      <div className={styles.header}>
+        <div className={styles.left}>Name</div>
+        <div className={styles.center}>Deaths</div>
+        <div className={styles.right}>Year</div>
+      </div>
+
+      <VirtualisedList
+        height={height}
+        itemData={volcanos}
+        itemCount={volcanos.length}
+        itemSize={60}
+        width="100%"
+      >
+        {Row}
+      </VirtualisedList>
+    </div>
+  );
+});
 
 export default List;
--- src/components/List/ListItem.jsx
+++ src/components/List/ListItem.jsx
@@ -3,16 +3,21 @@ import styles from "./styles.module.css";
 
 const noop = () => {};
 
-const ListItem = ({ year, name, deaths, id, onClick = noop }) => (
-  <div className={styles.item} onClick={() => onClick(id)}>
-    <div className={styles.left}>{name}</div>
-    {/* <div className={styles.center}>
-      {deaths > 0 ? (
-        <img className={styles.icon} src="/skull.svg" alt="Deathly" />
-      ) : null}
-    </div> */}
-    <div className={styles.right}>{year}</div>
-  </div>
-);
+const ListItem = ({ index, style, data, volcanoHandler }) => {
+  const { year, name, deaths, id } = data[index];
+  const onClick = volcanoHandler || noop;
+
+  return (
+    <div style={style} className={styles.item} onClick={() => onClick(id)}>
+      <div className={styles.left}>{name}</div>
+      <div className={styles.center}>
+        {deaths > 0 ? (
+          <img className={styles.icon} src="/skull.png" alt="Deathly" />
+        ) : null}
+      </div>
+      <div className={styles.right}>{year}</div>
+    </div>
+  );
+};
 
 export default ListItem;
